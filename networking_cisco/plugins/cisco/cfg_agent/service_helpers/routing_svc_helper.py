a7b7e80a38927d18191cbbaa3c86ab7cf3be5d01
Battye, Andrew <andrew.battye@sap.com>
2017-06-13 14:46:23 +0100

    Revert to ab6aec017e294353120be66519176cfd95765bfa

--- networking_cisco/plugins/cisco/cfg_agent/service_helpers/routing_svc_helper.py
+++ networking_cisco/plugins/cisco/cfg_agent/service_helpers/routing_svc_helper.py
@@ -196,7 +196,7 @@ class RoutingServiceHelper(object):
         self.removed_routers = set()
         self.sync_devices = set()
         self.sync_devices_attempts = 0
-        self.fullsync = self.conf.cfg_agent.enable_full_sync
+        self.fullsync = True
         self.sync_routers_chunk_size = (
             cfg.CONF.cfg_agent.max_device_sync_batch_size)
         self.topic = '%s.%s' % (c_constants.CFG_AGENT_L3_ROUTING, host)
@@ -254,9 +254,6 @@ class RoutingServiceHelper(object):
             routers = []
             removed_routers = []
             all_routers_flag = False
-
-            LOG.info("Checking full sync state: %s",self.conf.cfg_agent.enable_full_sync)
-
             if self.fullsync:
                 LOG.debug("The fullsync flag is set. Starting complete sync")
                 # Setting all_routers_flag and clear the global full_sync flag
@@ -271,8 +268,6 @@ class RoutingServiceHelper(object):
                 if routers is not None:
                     self._cleanup_invalid_cfg(routers)
             else:
-
-
                 if self.updated_routers:
                     router_ids = list(self.updated_routers)
                     LOG.debug("Updated routers: %s", router_ids)
