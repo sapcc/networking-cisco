28b24934c062a73f41d0cd55b13a87210681fb61
Battye, Andrew <andrew.battye@sap.com>
2017-07-17 11:02:59 +0100

    Support manual sync/delete of global routers and individual global router interfaces

--- networking_cisco/plugins/cisco/cfg_agent/device_drivers/asr1k/asr1k_cli_utils.py
+++ networking_cisco/plugins/cisco/cfg_agent/device_drivers/asr1k/asr1k_cli_utils.py
@@ -6,7 +6,7 @@ ACTION_MODULE = 'networking_cisco.plugins.cisco.cfg_agent.device_drivers.asr1k.c
 class Execute(argparse.Action):
     def __init__(self, option_strings, dest, nargs=None, **kwargs):
         super(Execute, self).__init__(option_strings, dest, **kwargs)
-        self.actions = {"validate":"validate.Validate","sync":"sync.Sync","delete":"delete.Delete"}
+        self.actions = {"validate":"validate.Validate","sync":"sync.Sync","delete":"delete.Delete","delete-global":"delete_global.DeleteGlobal","sync-global":"sync_global.SyncGlobal"}
 
     def __call__(self, parser, namespace, values, option_string=None):
         action = self.actions.get(values)
@@ -21,10 +21,17 @@ class Execute(argparse.Action):
 def main():
     parser = argparse.ArgumentParser(prog='asr1k_utils', description='Operations utilities for ASR1k driver.')
 
-    parser.add_argument('router_id',
-                       help='router id',action='store')
     parser.add_argument('command',
-                       help='command to execute',action=Execute,choices=["validate", "sync","delete"])
+                       help='command to execute',action=Execute,choices=["validate", "sync", "delete", "sync-global", "delete-global"])
+
+    parser.add_argument('--router-id',dest='router_id',
+                       help='router id',action='store')
+
+    parser.add_argument('--port-id',dest='port_id',
+                       help='Port of global router to sync')
+
+    parser.add_argument('--confirm',dest='confirm', action='store_true',
+                       help='Confirm high risk action')
 
     parser.add_argument('--config-file', dest='config', action='append',
                        default=["/etc/neutron/plugins/cisco/cisco_cfg_agent.ini", "/etc/neutron/neutron.conf"],
