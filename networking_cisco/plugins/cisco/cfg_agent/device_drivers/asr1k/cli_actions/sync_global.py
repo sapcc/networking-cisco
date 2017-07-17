5197d2e3014b19fb9f6d314dd1b06951150d82b2
Battye, Andrew <andrew.battye@sap.com>
2017-07-17 13:54:50 +0100

    Ensure same sort order for subnets, add as external and set up NAT seperately

--- networking_cisco/plugins/cisco/cfg_agent/device_drivers/asr1k/cli_actions/sync_global.py
+++ networking_cisco/plugins/cisco/cfg_agent/device_drivers/asr1k/cli_actions/sync_global.py
@@ -1,8 +1,10 @@
 
 import base_action
 from networking_cisco.plugins.cisco.cfg_agent.service_helpers import routing_svc_helper
+from networking_cisco.plugins.cisco.common import (cisco_constants as c_constants)
 import pprint as pp
 from oslo_log import log as logging
+
 
 LOG = logging.getLogger(__name__)
 
@@ -22,30 +24,35 @@ class SyncGlobal(base_action.BaseAction):
 
         if len(routers)==1:
             router = routers[0]
-            router['enable_snat']=False
-            driver = self.routing_service_helper.driver_manager.set_driver(router)
 
             print pp.pformat(router)
 
-            ri = routing_svc_helper.RouterInfo(self.router_id, router)
+            if router[routing_svc_helper.ROUTER_ROLE_ATTR] != c_constants.ROUTER_ROLE_GLOBAL:
+                print("Specified router is not a global Router")
+                exit(0)
 
+            driver = self.routing_service_helper.driver_manager.set_driver(router)
+            ri = routing_svc_helper.RouterInfo(self.router_id, router)
 
 
             for gw_port in router.get('_interfaces',[]):
                 if self.port_id is None or gw_port['id']==self.port_id:
-                    self.routing_service_helper._set_subnet_info(gw_port, gw_port['subnets'][0]['id'])
+                    port_subnets = sorted(gw_port['subnets'], key=routing_svc_helper.itemgetter('id'))
 
-                    self.routing_service_helper._internal_network_added(ri, gw_port, gw_port)
+                    self.routing_service_helper._set_subnet_info(gw_port, port_subnets[0]['id'])
+
+                    self.routing_service_helper._external_gateway_added(ri, gw_port)
 
                     # Process the secondary subnets. Only router ports of global
                     # routers can have multiple ipv4 subnets since we connect such
                     # routers to external networks using regular router ports.
-                    for gw_port_sn in gw_port['subnets'][1:]:
+                    for gw_port_sn in port_subnets[1:]:
                          self.routing_service_helper._set_subnet_info(gw_port, gw_port_sn['id'], is_primary=False)
-                         self.routing_service_helper._internal_network_added(ri, gw_port, gw_port)
+                         self.routing_service_helper._external_gateway_added(ri, gw_port)
 
 
-
+                    itfc_name = driver._get_interface_name_from_hosting_port(gw_port)
+                    driver._add_interface_nat(itfc_name, 'outside')
 
             print("Global Router {} synced with hosting device".format(self.router_id))
 
